2ff5e3de6cd0870ee0e6adff50429440ea93687c
pancongwen <peter.pan@dorry.space>
2018-02-20 15:53:42 +0800

    add deploy mongoDB

---
+++ deploy.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+# stop/start mongoDB
+
+DB_action(){
+	action="$1"
+	sudo systemctl $action mongodb.service
+}
+
+
+Parameter_judge() {
+	correct_num="$1"
+	real_num="$2"
+	
+	if [ $correct_num != $real_num ]; then
+		Usage
+	fi
+}
+
+
+Usage(){
+	echo "Usage: $0 { start | stop | restart | status }"
+	exit 1
+}
+
+Install() {
+	for action in start enable status
+	do 
+		DB_action $action
+	done
+}
+
+Uninstall() {
+	for action in stop disable status
+	do 
+		DB_action $action
+	done
+}
+
+
+case "$1" in 
+	start)
+	Parameter_judge 1 $#
+	Install start
+	;;
+
+	stop)
+	Parameter_judge 1 $#
+	Uninstall stop
+	;;
+
+	restart)
+	Parameter_judge 1 $#
+	DB_action restart
+	;;
+
+	status)
+	Parameter_judge 1 $#
+	DB_action status
+	;;
+
+	*)
+	Usage
+	;;
+esac
+
