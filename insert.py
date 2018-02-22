90bf271a467c716ec1a3695fc709b242677fa673
peter.pan <peter.pan@gnomito.com>
2018-02-22 21:43:18 +0800

    add insert function

--- insert.py
+++ insert.py
@@ -5,20 +5,10 @@ import json
 
 client = MongoClient( '127.0.0.1', 27017 )
 db = client['pan1']
-collection = db['info']
+collection = db['resume']
 
-file = json.loads(open("tamplate.json").read())
-print(file)
-
+#def insert_doc(file_name):
+file = json.loads(open("resume.json").read())
 ID = collection.insert_one(file).inserted_id
 ID
-
-print(collection.find_one())
-
-
-#def add_db(DB_name):
-#	DB_name = client[DB_name]
-
-#def add_collection(DB_name,Collection_name):
-#	Collection_name = DB_name[Collection_name]
 
